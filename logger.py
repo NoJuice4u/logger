8c5bfb23dabb2a8fe7dbff09a4db493ed54f4d2e
Wayne Chen <jungwnr@hotmail.com>
2019-12-28 22:50:18 -0800

    More Colors!

--- logger.py
+++ logger.py
@@ -4,8 +4,10 @@ import os
 os.system('')
 RED = '\033[91m'   # mode 31 = red forground
 GREEN = '\033[92m' 
+YELLOW = '\033[93m' 
 RESET = '\033[0m'  # mode 0  = reset
 
 def log(category, message):
     now = datetime.datetime.now()
     print("[" + GREEN + now.strftime('%Y-%m-%dT%H:%M:%S.%f') + RESET + "]\t[" + category + "]" + "\t" + message)
+    
